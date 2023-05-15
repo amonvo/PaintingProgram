e0152366e141fd93d364222295640b9abad14611
AmonV <vojtechamon@gmail.com>
2023-05-15 14:02:31 +0200

    Add project files.

---
+++ PaintingProgram/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PaintingProgram
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.colorButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(12, 12);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(75, 23);
+            this.colorButton.Text = "Barva";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(93, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.Text = "Vymazat";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(174, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.Text = "Uložit";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.colorButton);
+            this.Name = "MainForm";
+            this.Text = "Program pro malování";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ PaintingProgram/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PaintingProgram
+{
+    public partial class MainForm : Form
+    {
+        private bool isDrawing;
+        private Point previousPoint;
+        private Pen currentPen;
+        private Bitmap drawingBitmap;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            isDrawing = false;
+            currentPen = new Pen(Color.Black, 2); // Výchozí barva pera: černá, tloušťka: 2
+            drawingBitmap = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            previousPoint = e.Location;
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = Graphics.FromImage(drawingBitmap))
+                {
+                    g.DrawLine(currentPen, previousPoint, e.Location);
+                }
+                previousPoint = e.Location;
+                Invalidate(); // Vynucení překreslení plátna
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                currentPen.Color = colorDialog.Color;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(drawingBitmap))
+            {
+                g.Clear(Color.White);
+            }
+            Invalidate(); // Vynucení překreslení plátna
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    drawingBitmap.Save(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(drawingBitmap, Point.Empty);
+            base.OnPaint(e);
+        }
+    }
+}
